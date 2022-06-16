4c7cab6959e2329fe0dec358b9716da2e21c953b
andreayadi <andreayadi@outlook.com>
2022-06-16 17:58:48 +0700

    Bug Fixing

--- src/Pages/ChangePassword/ChangePassword.jsx
+++ src/Pages/ChangePassword/ChangePassword.jsx
@@ -3,7 +3,7 @@ import LayoutHome from "../../Layouts/Home/Home";
 import { Link } from "react-router-dom";
 import { Wrappers, CenterWrapper } from "./StyledChangePassword";
 import TagInformation from "../../Components/TextBox/TagInformation";
-import DetailsInformation from "../../Components/TextBox/DetailsInformation";
+// import DetailsInformation from "../../Components/TextBox/DetailsInformation";
 import InputPass from "../../Components/Form/InputPass";
 import Button from "../../Components/Button/Button";
 
@@ -11,7 +11,10 @@ const ChangePassword = () => {
   return (
     <LayoutHome>
       <Wrappers>
-        <TagInformation Title="Change Password" Desc="You must enter your current password and then type your new password twice." />
+        <TagInformation
+          Title="Change Password"
+          Desc="You must enter your current password and then type your new password twice."
+        />
       </Wrappers>
       <CenterWrapper>
         <InputPass Placeholder="Current Password" />
